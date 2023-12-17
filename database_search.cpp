283beb4d8098ba7dc0169c1309ad7afec7727b9b
tmaxell <tmaxelhard@gmail.com>
2023-12-17 21:50:55 +0300

    added data generator

--- database_search.cpp
+++ database_search.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <cstdlib> 
+#include <ctime>  
+
 using namespace std;
 
-// Структура для представления данных о студенте
 struct Student {
     string name;
     int id;
@@ -11,7 +13,6 @@ struct Student {
     string course;
 };
 
-// Структура узла AVL-дерева
 struct Node {
     Student data;
     Node* left;
@@ -19,46 +20,38 @@ struct Node {
     int height;
 };
 
-// Функция для получения высоты узла
 int height(Node* node) {
     if (node == nullptr)
         return 0;
     return node->height;
 }
 
-// Функция для вычисления баланса узла
 int getBalance(Node* node) {
     if (node == nullptr)
         return 0;
     return height(node->left) - height(node->right);
 }
 
-// Функция для поворота вправо поддерева, корень которого равен y
 Node* rightRotate(Node* y) {
     Node* x = y->left;
     Node* T2 = x->right;
 
-    // Выполнение поворота
     x->right = y;
     y->left = T2;
 
-    // Обновление высот узлов
     y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
 
     return x;
 }
 
-// Функция для поворота влево поддерева, корень которого равен x
 Node* leftRotate(Node* x) {
     Node* y = x->right;
     Node* T2 = y->left;
 
-    // Выполнение поворота
     y->left = x;
     x->right = T2;
 
-    // Обновление высот узлов
     x->height = max(height(x->left), height(x->right)) + 1;
     y->height = max(height(y->left), height(y->right)) + 1;
 
@@ -114,7 +107,7 @@ Node* insert(Node* root, Student data) {
     return root;
 }
 
-// Функция для поиска студента по номеру пропуска
+//поиск по пропуску
 Node* searchById(Node* root, int id) {
     if (root == nullptr || root->data.id == id)
         return root;
@@ -125,56 +118,87 @@ Node* searchById(Node* root, int id) {
         return searchById(root->right, id);
 }
 
-// Функция для поиска студента по ФИО
 void searchByName(Node* root, const string& name) {
     if (root == nullptr)
         return;
 
     searchByName(root->left, name);
 
-    // Поиск по ФИО, можно использовать любое подходящее условие
+    //поиск по имени
     if (root->data.name == name)
-        cout << "ID: " << root->data.id << ", Name: " << root->data.name << ", BirthDate: " << root->data.birthDate
-             << ", Dormitory: " << root->data.dormitory << ", Course: " << root->data.course << endl;
+        cout << "ID: " << root->data.id << ", Имя: " << root->data.name << ", Дата рождения: " << root->data.birthDate
+             << ", Общежитие: " << root->data.dormitory << ", Курс: " << root->data.course << endl;
 
     searchByName(root->right, name);
 }
 
-// Вспомогательная функция для вывода дерева в виде отсортированного списка
 void inOrderTraversal(Node* root) {
     if (root != nullptr) {
         inOrderTraversal(root->left);
-        cout << "ID: " << root->data.id << ", Name: " << root->data.name << ", BirthDate: " << root->data.birthDate
-             << ", Dormitory: " << root->data.dormitory << ", Course: " << root->data.course << endl;
+        cout << "ID: " << root->data.id << ", Имя: " << root->data.name << ", Дата рождения: " << root->data.birthDate
+             << ", Общежитие: " << root->data.dormitory << ", Курс: " << root->data.course << endl;
         inOrderTraversal(root->right);
     }
 }
 
-int main() {
-    Node* root = nullptr;
+//генераторы
+string generateRandomName() {
+    string names[] = {"Иван", "Денис", "Артём", "Тимофей", "Шамиль", "Александр", "Виктор", "Егор", "Григорий", "Татьяна", "Виктория", "Диана", "Елизавета", "Василиса", "Дарья", "Светлана"};
+    return names[rand() % (sizeof(names) / sizeof(names[0]))];
+}
 
-    // Пример вставки данных о студентах
-     root = insert(root, {"John Doe", 1, "01/01/1990", "Dorm 101", "1st year, Computer Science, University A"});
-    root = insert(root, {"Jane Doe", 2, "02/02/1991", "Dorm 102", "2nd year, Electrical Engineering, University B"});
-    root = insert(root, {"Bob Smith", 3, "03/03/1992", "Dorm 103", "3rd year, Mechanical Engineering, University C"});
+string generateRandomBirthDate() {
+    int day = rand() % 28 + 1;
+    int month = rand() % 12 + 1;
+    int year = rand() % 10 + 1990; // Год от 1990 до 1999
+    return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+}
+
+Student generateRandomStudent(int id) {
+    return {
+        generateRandomName(),
+        id,
+        generateRandomBirthDate(),
+        "Общежите " + to_string(rand() % 18 + 1),
+        to_string(rand() % 5 + 1) + "th year, " +
+        "Факультет " + to_string(rand() % 10 + 1) + ", " +
+        "Университет " + to_string(rand() % 3 + 1)
+    };
+}
+
+Node* generateAndInsertData(Node* root, int numRecords) {
+    srand(static_cast<unsigned>(time(0)));
+
+    for (int i = 1; i <= numRecords; ++i) {
+        Student student = generateRandomStudent(i);
+        root = insert(root, student);
+    }
+
+    return root;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Node* root = nullptr;
+    root = generateAndInsertData(root, 100000);
+
+    // Пример поиска по ФИО
+    string searchName = "Иван";
+    cout << "Студенты, найденные по имени " << searchName << ":" << endl;
+    searchByName(root, searchName);
 
     // Пример поиска по номеру пропуска
     int searchId = 2;
     Node* foundById = searchById(root, searchId);
     if (foundById != nullptr) {
-        cout << "Student found by ID " << searchId << ": " << foundById->data.name << endl;
+        cout << "Студенты, найденные по ID " << searchId << ": " << foundById->data.name << endl;
     } else {
-        cout << "Student with ID " << searchId << " not found." << endl;
+        cout << "Студенты с заданным ID " << searchId << " не найдены." << endl;
     }
 
-    // Пример поиска по ФИО
-    string searchName = "Jane Doe";
-    cout << "Students found by name " << searchName << ":" << endl;
-    searchByName(root, searchName);
-
-    // Пример вывода всего дерева
-    cout << "All students in sorted order:" << endl;
-    inOrderTraversal(root);
+   // cout << "All students in sorted order:" << endl;
+   //  inOrderTraversal(root);
 
     return 0;
 }
